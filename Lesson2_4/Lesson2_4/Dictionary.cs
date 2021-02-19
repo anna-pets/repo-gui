69b48e11bb3d3de1cc04bc0e2374ec0b2e580b7d
Annette <Anna.kozhushko@gmail.com>
2021-02-19 22:16:16 +0200

    lesson2_4

---
+++ Lesson2_4/Lesson2_4/Dictionary.cs
@@ -0,0 +1,6 @@
+﻿namespace Lesson2_4
+{
+	internal class Dictionary<T1, T2, T3>
+	{
+	}
+}
---
+++ Lesson2_4/Lesson2_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson2_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			var check = new List<(DateTime Date, string Name, double Price)>();
+
+
+			check.Add((Date: DateTime.Now, Name: "молоко", Price: 30));
+			check.Add((DateTime.Now, "капуста", 3));
+			check.Add((DateTime.Now, "банан", 55.50));
+			check.Add((DateTime.Now, "яблоко", 43.50));
+			check.Add((DateTime.Now, "сыр", 220));
+
+			foreach (var keyValue in check)
+			{
+				Console.WriteLine(keyValue.Date + "     " + keyValue.Name + "     " + keyValue.Price);
+			}
+		}
+	}
+}
