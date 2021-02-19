3232dbd2a0c3d8b30c2868c45199d74cf086b994
Annette <Anna.kozhushko@gmail.com>
2021-02-19 22:16:03 +0200

    lesson2_3

---
+++ Lesson2_3/Lesson2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson2_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.Write("Введите число: ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			if (a % 2 == 0)
+			{
+				Console.WriteLine($"Число чётное");
+			}
+			else
+			{
+				Console.WriteLine($"Число нечётное");
+			}
+		}
+	}
+}
