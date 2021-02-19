52c2a9d0abee7bad983e5ed1e682ff327ce49a32
Annette <Anna.kozhushko@gmail.com>
2021-02-19 22:13:52 +0200

    lesson2_1

---
+++ lesson2_1/lesson2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.Write("Введите максимальную температуру за сутки: ");
+			int max = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Введите минимальную температуру за сутки: ");
+			int min = Convert.ToInt32(Console.ReadLine());
+
+			int average = (min + max) / 2;
+
+			Console.WriteLine( ("Средняя температура за сутки= ")  + (average));
+
+
+
+		}
+	}
+}
